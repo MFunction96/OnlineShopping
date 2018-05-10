eeebe794a0d9b14e47d079af85ae52aa4010dbca
MFunction <MFunction96@outlook.com>
2018-05-10 22:30:14 +0800

    Fix webpack script

--- web/js/webpack.js
+++ web/js/webpack.js
@@ -1,6 +1,5 @@
-import "../css/site.css"
-import "bootstrap/dist/css/bootstrap.min.css"
+import "../css/site.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 global.jQuery = require("jquery");
 require("expose-loader?$!jquery");
-import "bootstrap"
-require("site");
+import "bootstrap";
