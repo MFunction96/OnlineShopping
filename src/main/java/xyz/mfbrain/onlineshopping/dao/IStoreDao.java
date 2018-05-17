f6847bd1f790fc400074123bcad181e8d0503085
ChrisYoung96 <yanghuaan96423@outlook.com>
2018-05-17 22:40:52 +0800

    添加数据库实体操纵累

---
+++ src/main/java/xyz/mfbrain/onlineshopping/dao/IStoreDao.java
@@ -0,0 +1,11 @@
+package xyz.mfbrain.onlineshopping.dao;
+
+/**
+ * @program: OnlineShopping
+ * @description: 数据库商铺操作
+ * @author: ChrisYoung
+ * @create: 2018-05-17 22:35
+ **/
+
+public interface IStoreDao {
+}
