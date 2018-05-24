81ece51d1ebb9e0bc7e992df68773f89da6de87f
lingyu duan <hlys0625@outlook.com>
2018-05-24 09:52:35 +0800

    添加注册模块（部分）

--- src/main/java/xyz/mfbrain/onlineshopping/action/LoginSeverlet.java
+++ src/main/java/xyz/mfbrain/onlineshopping/action/LoginSeverlet.java
@@ -1,4 +1,14 @@
 package xyz.mfbrain.onlineshopping.action;
 
-public class LoginSeverlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginSeverlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost( req, resp );
+    }
 }
