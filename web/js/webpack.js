cbf46b472163e710181bbae8f302105886b2219d
lingyu duan <hlys0625@outlook.com>
2018-05-25 15:05:07 +0800

    修改引用 修正配置文件 更新注册界面（基础

--- web/js/webpack.js
+++ web/js/webpack.js
@@ -1,4 +1,6 @@
 import "../css/site.css"
+import "../css/signin.css"
+import "../css/register.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 global.jQuery = require("jquery")
 require("expose-loader?$!jquery")
--- webpack.config.js
+++ webpack.config.js
@@ -39,23 +39,23 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use:
+                use: [
                     'style-loader',
                     'css-loader'
                 ]
-            }
+            },
             {
                 test: /\.(png|svg|jpg|gif)$/,
                 use: [
                     'file-loader'
                 ]
-            }
+            },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use: [
                     'file-loader'
                 ]
-            }
+            },
             {
                 test: /\.xml$/,
                 use: [
