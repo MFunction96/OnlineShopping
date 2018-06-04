0a12e5f0aebd9e1fe38f51b856bfb804a368afee
ChrisYoung96 <yanghuaan96423@outlook.com>
2018-06-04 10:32:07 +0800

    尝试实现动态加载商家方法

--- src/main/java/xyz/mfbrain/onlineshopping/action/Restaurantservlet.java
+++ src/main/java/xyz/mfbrain/onlineshopping/action/Restaurantservlet.java
@@ -1,11 +1,15 @@
 package xyz.mfbrain.onlineshopping.action;
 
+import xyz.mfbrain.onlineshopping.bean.StoreBean;
+import xyz.mfbrain.onlineshopping.biz.StoreService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet ("/chooseRestaurant")
 public class Restaurantservlet extends HttpServlet {
@@ -16,6 +20,9 @@ public class Restaurantservlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        StoreService ss=new StoreService();
+        ArrayList<StoreBean> stores=ss.getAllStores();
+        req.setAttribute("stores",stores);
         req.getRequestDispatcher( "chooseRestaurant.jsp" ).forward( req,resp );
     }
 }
